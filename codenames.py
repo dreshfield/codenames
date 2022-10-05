dee81b189785b75d36b5b4d788d3dcc720fa1017
David Dreshfield <david@dreshfield.com>
2022-10-04 20:53:22 -0400

    Add path to allow script to be run from anywhere; print results in uppercase

--- codenames.py
+++ codenames.py
@@ -1,5 +1,7 @@
-import itertools
-import random
+import itertools, random
+import pathlib as p
+
+script_dir = p.Path.cwd()
 
 def load_words(filename):
     with open(filename) as f:
@@ -7,8 +9,8 @@ def load_words(filename):
     return ([word for word, count in words],
             list(itertools.accumulate(int(count) for word, count in words)))
 
-adjs, adjs_weights = load_words('words/adj.txt')
-nouns, nouns_weights = load_words('words/noun.txt')
+adjs, adjs_weights = load_words(p.Path.joinpath(script_dir, 'words/adj.txt'))
+nouns, nouns_weights = load_words(p.Path.joinpath(script_dir, 'words/noun.txt'))
 
 for _ in range(20):
     while True:
@@ -16,4 +18,4 @@ for _ in range(20):
         noun = random.choices(nouns, cum_weights=nouns_weights)[0]
         if len(adj) >= 3 and len(noun) >= 3:
             break
-    print(adj, noun)
+    print(str.upper(adj), str.upper(noun))

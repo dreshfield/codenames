a9dfcf0345af48ebb849394a69f727cfe73de78d
David Dreshfield <david@dreshfield.com>
2024-01-10 07:01:40 -0500

    Minor corrections

--- codenames.py
+++ codenames.py
@@ -6,7 +6,7 @@ import pathlib as p
 
 parser = ap.ArgumentParser(description='Generates random codenames in the style of U.S. military/intelligence operations')
 parser.add_argument('-a', '--alpha', dest='alpha', action='store_true', help='display results in alphabetical, rather than random, order')
-parser.add_argument('-n', '--number', type=int, dest='n', action='store', nargs='?', default=20, help='specify number of results to return (default: 20)')
+parser.add_argument('-n', '--number', type=int, dest='n', action='store', help='specify number of results to return (default: 20)')
 args = parser.parse_args()
 
 script_dir = p.Path(__file__).resolve(strict=True).parent
@@ -21,7 +21,10 @@ adjs, adjs_weights = load_words(p.Path.joinpath(script_dir, 'words/adj.txt'))
 nouns, nouns_weights = load_words(p.Path.joinpath(script_dir, 'words/noun.txt'))
 names_list = []
 
-n = args.n
+if not args.n:
+    n = 20
+else:
+    n = args.n
 
 # TODO: This works, but can probably be done more cleanly
 for _ in range(n):
